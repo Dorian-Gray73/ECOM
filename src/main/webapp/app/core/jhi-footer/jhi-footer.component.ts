b2a1356f7a4a59c987fabce7c5d551a1b132e31f
Barbara Prapant <bab38prapant@hotmail.fr>
2022-09-09 16:29:54 +0200

    style(Logo) : Footer et Header correction

--- src/main/webapp/app/core/jhi-footer/jhi-footer.component.ts
+++ src/main/webapp/app/core/jhi-footer/jhi-footer.component.ts
@@ -1,4 +1,13 @@
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component
-export default class JhiFooter extends Vue {}
+export default class JhiFooter extends Vue {
+  //Button return to navbar
+  scrollMeTo(refName) {
+    var element = this.$refs[refName];
+    if (element instanceof HTMLElement) {
+      var top = element.offsetTop;
+    }
+    window.scrollTo(0, top);
+  }
+}
