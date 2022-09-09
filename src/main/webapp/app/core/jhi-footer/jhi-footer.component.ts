b088fb46c264f3e9907003d6de988c685e333779
Barbara Prapant <bab38prapant@hotmail.fr>
2022-09-09 16:54:44 +0200

    style(Logo) : fix let to const

--- src/main/webapp/app/core/jhi-footer/jhi-footer.component.ts
+++ src/main/webapp/app/core/jhi-footer/jhi-footer.component.ts
@@ -3,9 +3,9 @@ import { Component, Vue } from 'vue-property-decorator';
 @Component
 export default class JhiFooter extends Vue {
   //Button return to navbar
-  scrollMeTo(refName) {
-    let element = this.$refs[refName];
-    let top;
+  public scrollMeTo(refName) {
+    const element = this.$refs[refName];
+    let top = null;
     if (element instanceof HTMLElement) {
       top = element.offsetTop;
     }
