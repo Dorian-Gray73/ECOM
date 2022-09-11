d9b14799608f8acffa4083544adecffc1a958b42
Barbara Prapant <bab38prapant@hotmail.fr>
2022-09-11 17:04:16 +0200

    style(Recherche) : fix css

--- src/main/webapp/app/core/recherche/recherche.vue
+++ src/main/webapp/app/core/recherche/recherche.vue
@@ -2,57 +2,87 @@
   <div id="pageRecherche">
     <span id="title">Résultats de la recherche</span>
     <hr id="separate" />
-    <b-input-group id="searchBar">
-      <font-awesome-icon icon="fa-solid fa-magnifying-glass" id="iconSearchBar" />
-      <b-form-input placeholder="Nom d'une monture" id="inputSearchBar"></b-form-input>
-    </b-input-group>
+    <div id="resultat">
+      <b-input-group id="searchBar">
+        <font-awesome-icon icon="fa-solid fa-magnifying-glass" id="iconSearchBar" />
+        <b-form-input placeholder="Nom d'une monture" id="inputSearchBar"></b-form-input>
+      </b-input-group>
 
-    <div id="containerSearch">
-      <b-container id="affichageSearch">
-        <b-row>
-          <b-col>
-            <div class="cardPhoto">
-              <div id="photo"></div>
-              <div id="nomMonture">Lunette</div>
-              <div id="prixMonture">130 euros</div>
-            </div>
-          </b-col>
-          <b-col>
-            <div class="cardPhoto">
-              <div id="photo"></div>
-              <div id="nomMonture">Lunette</div>
-              <div id="prixMonture">130 euros</div>
-            </div>
-          </b-col>
-          <b-col>
-            <div class="cardPhoto">
-              <div id="photo"></div>
-              <div id="nomMonture">Lunette</div>
-              <div id="prixMonture">130 euros</div>
-            </div>
-          </b-col>
-        </b-row>
-      </b-container>
-      <div id="filtres">
-        <div>Couleur</div>
-        <b-form-group label="Form-checkbox-group stacked checkboxes" v-slot="{ ariaDescribedby }">
-          <b-form-checkbox-group
-            v-model="selected"
-            :options="options"
-            :aria-describedby="ariaDescribedby"
-            name="flavour-2a"
-            stacked
-          ></b-form-checkbox-group>
-        </b-form-group>
-        <div>Prix</div>
+      <div id="containerSearch">
+        <div id="affichageSearch">
+          <div class="cardPhoto">
+            <div id="photo"></div>
+            <div id="nomMonture">Lunette</div>
+            <div id="prixMonture">130 euros</div>
+          </div>
+
+          <div class="cardPhoto">
+            <div id="photo"></div>
+            <div id="nomMonture">Lunette</div>
+            <div id="prixMonture">130 euros</div>
+          </div>
+          <div class="cardPhoto">
+            <div id="photo"></div>
+            <div id="nomMonture">Lunette</div>
+            <div id="prixMonture">130 euros</div>
+          </div>
+          <div class="cardPhoto">
+            <div id="photo"></div>
+            <div id="nomMonture">Lunette</div>
+            <div id="prixMonture">130 euros</div>
+          </div>
+
+          <div class="cardPhoto">
+            <div id="photo"></div>
+            <div id="nomMonture">Lunette</div>
+            <div id="prixMonture">130 euros</div>
+          </div>
+
+          <div class="cardPhoto">
+            <div id="photo"></div>
+            <div id="nomMonture">Lunette</div>
+            <div id="prixMonture">130 euros</div>
+          </div>
+          <div class="cardPhoto">
+            <div id="photo"></div>
+            <div id="nomMonture">Lunette</div>
+            <div id="prixMonture">130 euros</div>
+          </div>
+
+          <div class="cardPhoto">
+            <div id="photo"></div>
+            <div id="nomMonture">Lunette</div>
+            <div id="prixMonture">130 euros</div>
+          </div>
+          <div class="cardPhoto">
+            <div id="photo"></div>
+            <div id="nomMonture">Lunette</div>
+            <div id="prixMonture">130 euros</div>
+          </div>
+        </div>
+        <div id="filtres">
+          <div>Couleur</div>
+          <b-form-group label="Form-checkbox-group stacked checkboxes" v-slot="{ ariaDescribedby }">
+            <b-form-checkbox-group
+              v-model="selected"
+              :options="options"
+              :aria-describedby="ariaDescribedby"
+              name="flavour-2a"
+              stacked
+            ></b-form-checkbox-group>
+          </b-form-group>
+          <div>Prix</div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <style scoped>
+.col {
+  padding-right: 0;
+}
+
 #searchBar {
-  width: 85vw;
-  margin: 0 auto;
   background-color: #ffffff;
   display: flex;
   gap: 16px;
@@ -71,41 +101,35 @@
 }
 
 #containerSearch {
+  flex: 1;
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: flex-start;
   padding: 0px;
   gap: 32px;
-  margin: 20px auto;
-
-  width: 1201px;
   height: 1186px;
+  margin: 32px 0;
 }
 
 #affichageSearch {
+  flex: 6;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
+  justify-content: space-between;
+  flex-wrap: wrap;
   padding: 0px;
   gap: 32px;
-
-  width: 964px;
   height: 1186px;
 }
 
 #filtres {
+  flex: 1;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: flex-start;
   padding: 32px;
   gap: 64px;
-
-  width: 205px;
-  height: 656px;
-
   background: #ffffff;
 }
 
@@ -146,5 +170,12 @@
   line-height: 23px;
 
   color: #5b85aa;
+}
+
+#resultat {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  margin: 20px 128px;
 }
 </style>
